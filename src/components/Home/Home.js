ac932298639a526da66932d22c5ff07b46ffe7fd
GopalSingh019 <gs0190198@gmail.com>
2022-11-28 11:01:07 +0530

    push responsive changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,25 +5,25 @@ import './Home.css';
 function Home() {
   return (
     <section className='home__container'>
-        <img className='home__img' src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"/>
-        <div className='home__rows'>
-            <Product title="Atomic Habits by James Clear"
-            rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99"/>
-            <Product title="Atomic Habits by James Clear"
-            rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99"/>
-        </div>
-        <div className='home__rows'>
+      <img className='home__img' src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" />
+      <div className='home__rows'>
         <Product title="Atomic Habits by James Clear"
-            rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99"/>
-            <Product title="Atomic Habits by James Clear"
-            rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99"/>
-            <Product title="Atomic Habits by James Clear"
-            rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99"/>
-        </div>
-        <div className='home__rows'>
+          rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99" />
         <Product title="Atomic Habits by James Clear"
-            rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99"/>
-        </div>
+          rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99" />
+      </div>
+      <div className='home__rows home__rows2'>
+        <Product title="Atomic Habits by James Clear"
+          rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99" />
+        <Product title="Atomic Habits by James Clear"
+          rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99" />
+        <Product title="Atomic Habits by James Clear"
+          rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99" />
+      </div>
+      <div className='home__rows'>
+        <Product title="Atomic Habits by James Clear"
+          rating="4" img="https://m.media-amazon.com/images/I/91bYsX41DVL.jpg" price="229.99" />
+      </div>
     </section>
   )
 }
