d239a18df13bf09b23f11c27027d7e654bc98895
GopalSingh019 <gs0190198@gmail.com>
2023-01-19 22:05:24 +0530

    checkout

--- src/components/Checkout/checkout.js
+++ src/components/Checkout/checkout.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './checkout.css'
+import CheckoutProduct from './CheckoutProduct';
 
 function checkout() {
   return (
     <div>
         <header className='checkout__Header'><h2>Checkout</h2></header>
-        <main className='checkout__Container'></main>
+        <main className='checkout__Container'>
+          <div className='container'>
+            <div>
+            <div className='summary__header'>
+              <h3>ORDER SUMMARY</h3>
+            </div>
+            <div className='summary__container'>
+              <CheckoutProduct></CheckoutProduct>
+            </div>
+            </div>
+            <div>
+              <div className='payment__Header'><h3>PAYMENT OPTIONS</h3></div>
+              <div className='payment__Container'></div>
+            </div>
+          </div>
+        </main>
     </div>
   )
 }
