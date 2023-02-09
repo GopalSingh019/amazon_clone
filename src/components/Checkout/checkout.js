7ed92264517e035b2de044bf3f41d69166ff361e
GopalSingh019 <gs0190198@gmail.com>
2023-02-09 16:42:14 +0530

    stripe added

--- src/components/Checkout/checkout.js
+++ src/components/Checkout/checkout.js
@@ -3,14 +3,28 @@ import './checkout.css'
 import CheckoutProduct from './CheckoutProduct';
 import AddressForm from './AddressForm';
 import { useSelector } from 'react-redux';
+import { CardElement, Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { Button } from '@mui/material';
 
+const stripePromise=loadStripe('pk_test_51MZVocSDW6rpkpt9ETLUszQpj6b7z4qzoybS7LCFzTb3xabWMke6a95hzmpvj5ZINU8bEMjsRZJ5U7LgHslqxRhL00y8mnSKc7')
 
 function checkout() {
   const [openAdd,setOpenAdd]=useState(false);
-  const [address,setAddress]=useState();
+  const [address,setAddress]=useState('this');
   const items=useSelector(store=>store.Items.items);
   console.log(items);
   const closeModal=()=>setOpenAdd(false);
+
+  const options = {
+    // passing the client secret obtained from the server
+    clientSecret: '{{sk_test_51MZVocSDW6rpkpt9kw7ioajBEKwNGGf09ysfCLZyKmKKvEFmgBjjEWH6NnR2fpVdh69fIeTq6LFGyilIugiaYxXD00qjtC5SCG}}',
+  };
+
+  const handlePaySubmit=(e)=>{
+    e.preventDefault();
+    console.log('submitted');
+  }
   
   return (
     <div>
@@ -35,8 +49,11 @@ function checkout() {
             </div>
           </div>
           <div className='list__container'>
+            <Elements stripe={stripePromise} >
             <div className='payment__Header'><h3>PAYMENT OPTIONS</h3></div>
+            {address ? <form className='payment__form' onSubmit={handlePaySubmit}><label>Enter Card Details</label><CardElement/><Button variant="contained" type='submit'>Submit</Button></form>:null}
             <div className='payment__Container'></div>
+            </Elements>
           </div>
         </div>
       </main>
